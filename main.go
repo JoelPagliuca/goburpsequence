075118406b7fc89fc8f8a62a7bcf3814708a003c
JoelPagliuca <joelpagliuca@hotmail.com>
2018-09-07 08:41:47 +1000

    going to be using the exported proxy history items instead of the burpstate

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func usage() {
 func main() {
 	helpFlag := flag.Bool("h", false, "Display this help text")
 	plantServer := flag.String("server", "http://www.plantuml.com/plantuml/uml/", "server to render the plantuml document")
-	burpStateFile := flag.String("b", "", "burpstate file to convert")
+	burpItemsFile := flag.String("b", "", "burp saved items file to convert")
 	pcapFile := flag.String("p", "", "burpstate file to convert")
 	detailLevel := flag.Int("d", 1, "level of detail for the diagram {1|2|3}")
 	outputPNG := flag.String("output-png", "diagram.png", "output a PNG file (default)")
@@ -26,7 +26,7 @@ func main() {
 	}
 
 	fmt.Println("plantServer: " + *plantServer)
-	fmt.Println("burpStateFile: " + *burpStateFile)
+	fmt.Println("burpStateFile: " + *burpItemsFile)
 	fmt.Println("pcapFile: " + *pcapFile)
 	fmt.Println("detailLevel: " + string(*detailLevel))
 	fmt.Println("outputPNG: " + *outputPNG)
