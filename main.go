ab1b8fdace738d396f45f1399c2acef1e8eb0c8d
Joel Pagliuca <Joel.Pagliuca@cba.com.au>
2018-09-06 18:27:46 +1000

    started thinking about the flags I'll want

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
+
+func usage() {
+	fmt.Println("goburpsequence!")
+	fmt.Println("")
+	flag.PrintDefaults()
+}
 
 func main() {
-	fmt.Println("goburpsequence!")
+	helpFlag := flag.Bool("h", false, "Display this help text")
+	plantServer := flag.String("server", "http://www.plantuml.com/plantuml/uml/", "server to render the plantuml document")
+	burpStateFile := flag.String("b", "", "burpstate file to convert")
+	pcapFile := flag.String("p", "", "burpstate file to convert")
+	detailLevel := flag.Int("d", 1, "level of detail for the diagram {1|2|3}")
+	flag.Parse()
+
+	if *helpFlag {
+		usage()
+	}
+
+	fmt.Println("plantServer: " + *plantServer)
+	fmt.Println("burpStateFile: " + *burpStateFile)
+	fmt.Println("pcapFile: " + *pcapFile)
+	fmt.Println("detailLevel: " + string(*detailLevel))
 }
