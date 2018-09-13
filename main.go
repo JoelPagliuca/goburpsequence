7fcd549d27cd494ca67f1989abb09148dace71c4
Joel Pagliuca <pagliucajoel@gmail.com>
2018-09-13 22:45:09 +1000

    structs for reading in the burp file

--- main.go
+++ main.go
@@ -1,14 +1,46 @@
 package main
 
 import (
+	"encoding/xml"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"os"
 )
 
 func usage() {
 	fmt.Println("goburpsequence!")
 	fmt.Println("")
 	flag.PrintDefaults()
+}
+
+type Items struct {
+	XMLName xml.Name `xml:"items"`
+	Items   []Item   `xml:"item"`
+}
+
+type Item struct {
+	XMLName xml.Name `xml:"item"`
+	Host    string   `xml:"host"`
+	Method  string   `xml:"method"`
+	Status  int      `xml:"status"`
+	Path    string   `xml:"path"`
+}
+
+func ReadBurpFile(fileLocation string) Items {
+	xmlFile, err := os.Open(fileLocation)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer xmlFile.Close()
+
+	xmlFileBytes, _ := ioutil.ReadAll(xmlFile)
+
+	var items Items
+	xml.Unmarshal(xmlFileBytes, &items)
+
+	return items
 }
 
 func main() {
